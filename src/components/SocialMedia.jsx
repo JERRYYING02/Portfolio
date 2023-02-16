48a71f0ea954892f1d1c15b7c2ecdc13e269a986
JERRYYING02 <90186306+JERRYYING02@users.noreply.github.com>
2023-02-16 20:23:28 +0800

    my portfolio

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsTwitter, BsInstagram, BsGithub, BsYoutube } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <FaFacebookF />
     </div>
     <div>
-      <FaFacebookF />
+      <BsYoutube />
     </div>
     <div>
       <BsInstagram />
